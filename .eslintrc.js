078c6b5b9802531e70571ddf78e592b6333a6cc9
johntarus <tarusjohn96@gmail.com>
2022-09-03 18:54:01 +0300

    feat: configured prettier, eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+}
