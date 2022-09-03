52c0adab46e5c5497cdf1320d8d1014d8b34e966
johntarus <tarusjohn96@gmail.com>
2022-09-03 21:38:32 +0300

    conventional commits

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
         <div className="App">
             <h1>Hello World</h1>
             <p>testing</p>
+            <p>checking conventional commits</p>
         </div>
     )
 }
