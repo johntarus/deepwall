3025eac28522e3e1c03630f2ba474940862cb1e8
johntarus <tarusjohn96@gmail.com>
2022-09-04 19:22:49 +0300

    feat: configured tailwind css for styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1 className="text-red-400">Hello World</h1>
             <p>testing</p>
             <p>checking conventional commits</p>
         </div>
